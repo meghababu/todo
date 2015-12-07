e08a6e655f9dc3aedc631f5f770244c1167e9b93
megha <megha@rawdatatech.com>
2015-12-07 16:12:24 +0530

    todo app

--- test.js
+++ test.js
@@ -5,17 +5,11 @@ angular.module('myapp',[])
   console.log("test");
  $scope.login = function(){   
  console.log("Hai");
- // if $scope.name equals"megha" && $scope.pwd equals "megha" then
- 	
  		window.location.assign("index.html")
  	
- // window.location = "test.html";
 
-        // $location.path('/test.html');
     }
-  // $scope.name = function () {
-  //   return $scope.userName;
-  // };
+ 
 });
 
 
